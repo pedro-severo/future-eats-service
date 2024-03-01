22b9fc23966e47e14e6d71d844fdd00f45194f6c
Pedro Severo <phsevero94@gmail.com>
2024-03-01 15:42:49 +0000

    firestore setup

--- src/modules/user/database/UserDatabase.ts
+++ src/modules/user/database/UserDatabase.ts
@@ -2,11 +2,16 @@ import { Service } from "typedi";
 import { UserResponse } from './interfaces/UserResponse';
 import { Database } from "../../../shared/database";
 
+const usersCollectionName = 'users'
+
 @Service()
 export class UserDatabase extends Database {
-
     constructor() {
         super()
+    }
+
+    protected getCollectionName(): string {
+        return usersCollectionName;
     }
 
     async checkUserExistenceById(id: string): Promise<boolean> {
--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -1,11 +1,29 @@
+import { initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const serviceAccount = require('../../../../future-eats-service-5f069c811a09.json');
+
 export abstract class Database {
-    protected table = ""
+    protected db: FirebaseFirestore.Firestore;
+
+    constructor() {
+        initializeApp({
+            credential: cert(serviceAccount)
+        });
+        this.db = getFirestore();
+    }
+
+    protected abstract getCollectionName(): string;
 
     async insert(itemToAdd: any) {
         try {
-            return itemToAdd
+            const { id } = itemToAdd;
+            await this.db.collection(this.getCollectionName()).doc(id).set({
+                ...itemToAdd
+            });
+            return itemToAdd;
         } catch (e) {
-            throw new Error("Error in insert query.")
+            throw new Error("Error in insert query.");
         }
     }
 }
