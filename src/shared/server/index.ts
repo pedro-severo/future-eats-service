a67e63371f0cbbb0db61c2f4591029e4956c1662
Pedro Severo <phsevero94@gmail.com>
2024-04-18 17:34:43 +0100

    checkpoint

--- src/shared/server/index.ts
+++ src/shared/server/index.ts
@@ -2,7 +2,7 @@ import { Token } from 'typedi';
 import { UserRepository } from '../../modules/user/repository/UserRepository';
 import { UserDatabaseToken } from '../dependencies';
 
-// @ts-expect-error apollo-server callback
+// @ts-expect-error apollo-server middleware
 export const getServerContext = (req): IServerContext => {
     const token = req?.headers?.authorization || '';
     return { auth: { token }, userDatabaseContext: UserDatabaseToken };
